86f90c6cb0aa388e2b502b81c19dbf4bf71ca0ca
zhuzhe <zhuzheiswho@sina.com>
2012-05-23 18:00:51 +0800

    dev

--- lib/spider/spider.rb
+++ lib/spider/spider.rb
@@ -2,5 +2,17 @@ require File.expand_path('../imanhua.rb', __FILE__)
 
 module Spider
 
+	def create_agent
+		agent = Mechanize.new
+		agent.user_agent_alias = "Windows IE 7"
+		agent.request_headers = {
+			'Accept-Charset'=> 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+			'Accept-Language'=> 'en-US,en;q=0.8',
+			'Cache-Control'=> 'max-age=0',
+			'Accept-Encoding'=> 'gzip,deflate,sdch'
+		}
+		agent
+	end
+
 
 end
