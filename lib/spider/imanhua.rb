b018036c1cd890800f868b168e4292e9ea87ec7c
zhuzhe <zhuzheiswho@sina.com>
2012-06-01 11:17:25 +0800

    dev

--- lib/spider/imanhua.rb
+++ lib/spider/imanhua.rb
@@ -195,7 +195,9 @@ module Spider
 				'Accept-Charset'=> 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
 				'Accept-Language'=> 'en-US,en;q=0.8',
 				'Cache-Control'=> 'max-age=0',
-				'Accept-Encoding'=> 'gzip,deflate,sdch'
+				'Accept-Encoding'=> 'gzip,deflate,sdch',
+				'Connection' => 'keep-alive',
+				'User-Agent:' => 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.106 Safari/535.2'
 			}
 			agent
 		end
