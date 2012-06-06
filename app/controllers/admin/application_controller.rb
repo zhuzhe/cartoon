88076955971849a2233f03ae6729d60c645e4fa5
zhuzhe <zhuzheiswho@sina.com>
2012-06-06 09:48:38 +0800

    dev

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,15 @@
+class Admin::ApplicationController < ApplicationController
+  protect_from_forgery
+
+  USERS = { "zhuzhe" => "zhuzhe@manhua.com" }
+  
+   before_filter :authenticate
+  
+   private
+  
+   def authenticate
+     authenticate_or_request_with_http_digest do |username|
+       USERS[username]
+     end
+   end
+end
